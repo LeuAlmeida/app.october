6b1cf2e1606a5960bd40d24ae8a7ca44dbdeb197
LeuAlmeida <leo@webid.net.br>
2019-10-22 13:48:45 -0300

    Dashboard page

--- mobile/src/pages/Main/index.js
+++ mobile/src/pages/Main/index.js
@@ -1,77 +1,74 @@
 import React from 'react';
+import {StyleSheet, Text} from 'react-native';
 
 import {
-  SafeAreaView, ScrollView, View, Image, Text, ImageBackground, StyleSheet,
-} from 'react-native';
+  Container,
+  Background,
+  PerfilImg,
+  Perfil,
+  FullName,
+  BalanceCard,
+  CardTitle,
+  TitleText,
+  CardBody,
+  BalanceValue,
+  UniqueService,
+  ServiceIcons,
+  ServiceIcon,
+  ServiceTexts,
+  ServiceText,
+} from './style';
 
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#f9fafc',
-  },
-  bgImage: {
-    alignItems: 'center',
-    height: 175,
-    paddingHorizontal: 20,
-  },
-  perfilImg: {
-    width: 50,
-    height: 50,
-    borderRadius: 30,
-    resizeMode: 'cover',
-    marginRight: 10,
-  },
-  perfilView: {
-    flex: 1,
-    flexDirection: 'row',
-    marginTop: 15
-  },
-  fullName: {
-    fontWeight: 'bold',
-    flexDirection: 'column',
-  },
-  userName: {
-    fontWeight: 'bold',
-    flexWrap: 'wrap',
-    flexDirection: 'column',
-  },
-  balanceCard: {
-    backgroundColor: '#fff',
-    height: 175,
-    width: '80%',
-    alignSelf: 'center',
-    marginTop: -70
-  },
-  balanceTitle: {
+import perfilPic from '../../assets/img/perfil.jpg';
 
-  },
-
-});
-
-import Perfil from '../../assets/img/perfil.jpg';
+import allPays from '../../assets/img/icons/capital.png';
 
 const Main = () => (
+  <Container>
+    <Background
+      source={{
+        uri: 'https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/background.png',
+      }}>
+      <Perfil style={{alignSelf: 'flex-start'}}>
+        <PerfilImg source={perfilPic} />
+        <FullName>Léu Almeida</FullName>
+      </Perfil>
+    </Background>
+    <BalanceCard style={styles.defaultShadow}>
+      <CardTitle>
+        <TitleText>Total do empréstimo</TitleText>
+        <BalanceValue>R$ 4.380,00</BalanceValue>
+      </CardTitle>
 
-
-
-  <SafeAreaView style={styles.container}>
-    <ImageBackground
-    source={{
-      uri: 'https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/background.png',
-    }}
-    style={styles.bgImage}
-    resizeMode="cover"
-  >
-    <View style={styles.perfilView}>
-      <Image style={styles.perfilImg} source={Perfil} />
-      <Text style={styles.fullName}>Léu Almeida</Text>
-    </View>
-  </ImageBackground>
-    <View style={styles.balanceCard}>
-      <Text style={styles.balanceTitle}></Text>
-    </View>
-
-  </SafeAreaView>
-
+      <CardBody>
+        <UniqueService>
+          <ServiceIcon source={allPays} />
+          <ServiceText>Teste</ServiceText>
+        </UniqueService>
+        <UniqueService>
+          <ServiceIcon source={allPays} />
+          <ServiceText>Teste</ServiceText>
+        </UniqueService>
+        <UniqueService>
+          <ServiceIcon source={allPays} />
+          <ServiceText>Teste</ServiceText>
+        </UniqueService>
+      </CardBody>
+    </BalanceCard>
+  </Container>
 );
+
+const styles = StyleSheet.create({
+  defaultShadow: {
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 12,
+    },
+    shadowOpacity: 0.3,
+    shadowRadius: 16.0,
+    elevation: 24,
+  },
+});
 
 export default Main;
