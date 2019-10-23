f3d198d8f596ef27e67d1a96df5d24a54a8438e9
LeuAlmeida <leo@webid.net.br>
2019-10-23 14:05:44 -0300

    Create new pages and routes

---
+++ mobile/src/pages/Account/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+export default class Account extends Component {
+  render() {
+    return <View />;
+  }
+}
---
+++ mobile/src/pages/MySchool/index.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+export default class MySchool extends Component {
+  render() {
+    return <View />;
+  }
+}
