78e9c91a6c301893cda222256b805c404caae3b5
LeuAlmeida <leo@webid.net.br>
2019-10-23 18:57:38 -0300

    Update Statement index page

--- mobile/src/pages/Statement/index.js
+++ mobile/src/pages/Statement/index.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import {withNavigation} from 'react-navigation';
 
-import {View, ActivityIndicator, ImageBackground, TouchableOpacity, StyleSheet, Text} from 'react-native';
-import { Container, CardContent, CardIcon, CardDescription, CardTitle, CardValue, CardDate, CardPs } from './style';
+import {
+  View,
+  ActivityIndicator,
+  ImageBackground,
+  TouchableOpacity,
+  StyleSheet,
+  Text,
+} from 'react-native';
+import {
+  Container,
+  CardContent,
+  CardIconCircle,
+  CardIcon,
+  CardDescription,
+  CardTitle,
+  CardValue,
+  CardDate,
+  CardPs,
+} from './style';
 import Icon from 'react-native-vector-icons/MaterialIcons';
+
+import moneyBag from '../../assets/img/money-bag.png';
 
 class Main extends React.Component {
   constructor(props) {
@@ -38,30 +57,36 @@ class Main extends React.Component {
       );
     }
 
+    const value = this.props.navigation.getParam('value');
+    const dueDate = this.props.navigation.getParam('dueDate');
+
+
     return (
       <>
-        <ImageBackground source={require('../../assets/img/bg.jpg')} style={{width: '100%', height: '100%'}}>
+        <ImageBackground
+          source={require('../../assets/img/bg.jpg')}
+          style={{width: '100%', height: '100%'}}>
           <Container>
-            <TouchableOpacity>
-            <Icon
-              name="keyboard-arrow-left"
-              size={30}
-              color="#fff"
-            />
+            <TouchableOpacity onPress={() => this.props.navigation.navigate('Main')}>
+              <Icon name="keyboard-arrow-left" size={30} color="#fff" />
             </TouchableOpacity>
             <CardContent>
-              <CardIcon />
-              <CardDescription>You sucessfully</CardDescription>
-              <CardTitle>send money to Julia.</CardTitle>
+              <CardIconCircle>
+                <CardIcon source={moneyBag} />
+              </CardIconCircle>
+              <CardDescription>Sua fatura está disponível</CardDescription>
+              <CardTitle>para pagamento! :)</CardTitle>
 
-              <CardValue>R$ 400.00,00</CardValue>
+              <CardValue>{ value }</CardValue>
 
-              <CardDate>Vencimento 10/12/2019</CardDate>
+              <CardDate>Vencimento { dueDate }</CardDate>
 
-              <CardPs>Pagando agora você ganhará R$X de desconto</CardPs>
+              <CardPs>Pagando agora você ganhará R$0,32 de desconto no mês subsequente.</CardPs>
 
               <TouchableOpacity style={styles.pay}>
-                <Text>Done</Text>
+                <Text style={styles.payText}>
+                  Pagar fatura
+                </Text>
               </TouchableOpacity>
             </CardContent>
           </Container>
@@ -73,7 +98,19 @@ class Main extends React.Component {
 
 const styles = StyleSheet.create({
   pay: {
-
+    flex: 1,
+    backgroundColor: '#7b79fc',
+    marginBottom: 30,
+    width: '75%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderColor: '#7b79fc',
+    borderRadius: 50
+  },
+  payText: {
+    paddingVertical: 20,
+    color: '#FFF'
   }
 });
 
