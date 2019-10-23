bfce107763ced4efa6873de6b6cd0cc58cad0ace
LeuAlmeida <leo@webid.net.br>
2019-10-23 18:56:53 -0300

    Update the page using real data

--- mobile/src/pages/Main/index.js
+++ mobile/src/pages/Main/index.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { withNavigation } from 'react-navigation';
+import React, {Component} from 'react';
 import {
   StyleSheet,
   Text,
@@ -7,10 +6,10 @@ import {
   ActivityIndicator,
   ImageBackground,
   FlatList,
+  AsyncStorage,
 } from 'react-native';
 import {
   Container,
-  Background,
   PerfilImg,
   Perfil,
   FullName,
@@ -34,30 +33,28 @@ import perfilPic from '../../assets/img/perfil.jpg';
 import actualIcon from '../../assets/img/icons/actual.png';
 import scheduleIcon from '../../assets/img/icons/schedule.png';
 
-class Main extends React.Component {
+export default class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {isLoading: true};
   }
 
   componentDidMount() {
-    return (
-      fetch('http://www.mocky.io/v2/5c923b0932000029056bce39')
-        .then(response => response.json())
-        .then(responseJson => {
-          this.setState(
-            {
-              isLoading: false,
-              allData: responseJson,
-              dataSource: responseJson.installments,
-            },
-            function() {},
-          );
-        })
-        .catch(error => {
-          console.error(error);
-        })
-    );
+    return fetch('http://www.mocky.io/v2/5c923b0932000029056bce39')
+      .then(response => response.json())
+      .then(responseJson => {
+        this.setState(
+          {
+            isLoading: false,
+            allData: responseJson,
+            dataSource: responseJson.installments,
+          },
+          function() {},
+        );
+      })
+      .catch(error => {
+        console.error(error);
+      });
   }
 
   render() {
@@ -69,72 +66,79 @@ class Main extends React.Component {
       );
     }
 
+    const {navigate} = this.props.navigation;
+
     return (
       <Container>
-          <ImageBackground source={require('../../assets/img/bg.jpg')} style={{width: '100%', height: '100%'}}>
+        <ImageBackground
+          source={require('../../assets/img/bg.jpg')}
+          style={{width: '100%', height: '100%'}}>
           <Perfil style={{alignSelf: 'flex-start'}}>
             <PerfilImg source={perfilPic} />
             <FullName>Léu Almeida</FullName>
           </Perfil>
-        <BalanceCard style={styles.defaultShadow}>
-          <CardTitle>
-            <TitleText>Total do empréstimo</TitleText>
-              <BalanceValue>R$ 4.823,00</BalanceValue>
-          </CardTitle>
+          <BalanceCard style={styles.defaultShadow}>
+            <CardTitle>
+              <TitleText>Total do empréstimo</TitleText>
+              <BalanceValue>R$ {this.state.allData.amountTaken}</BalanceValue>
+            </CardTitle>
 
-          <CardBody>
-            <UniqueService />
-          </CardBody>
-        </BalanceCard>
+            <CardBody>
+              <UniqueService />
+            </CardBody>
+          </BalanceCard>
 
-        <QuickLine />
+          <QuickLine />
 
-        <StatementSection>
-          <Text style={{fontWeight: 'bold', color: '#282d33'}}>Faturas recentes</Text>
-          <Icon
+          <StatementSection>
+            <Text style={{fontWeight: 'bold', color: '#282d33'}}>
+              Faturas recentes
+            </Text>
+            <Icon
               name="keyboard-arrow-right"
               size={24}
               color="#282d33"
               style={styles.quickAccessIcon}
             />
-        </StatementSection>
-        <FlatList
-          horizontal
-          data={this.state.dataSource}
-          renderItem={({item}) => (
-            <AllStatements>
-              <IndividualStatement style={styles.defaultShadow}>
-                <StatementIcon source={item.payd ? actualIcon : scheduleIcon} />
-                <View style={{flexDirection: 'column'}}>
-                  <StatementTitle>
-                  {item.payd ? 'Fatura paga' : 'Fatura em aberto'}
-                  </StatementTitle>
-                  <StatementPrice>
-                    {item.formatedValue}
-                    <Text style={{color: '#73a8f6'}}> / Venc. {item.dueDate}</Text>
-                  </StatementPrice>
-                </View>
-              </IndividualStatement>
-            </AllStatements>
-          )}
-          keyExtractor={({id}, index) => id}
-        />
-
-        {/* <View style={{width: '90%'}}>
-            <FlatList
-              style={{paddingTop: 10}}
-              data={this.state.dataSource}
-              renderItem={({item}) => <Text style={{textAlign: 'center'}}>{item.formatedValue}, {item.dueDate}</Text>}
-              keyExtractor={({id}, index) => id}
-            />
-        </View> */}
-      </ImageBackground>
+          </StatementSection>
+          <FlatList
+            horizontal
+            data={this.state.dataSource}
+            renderItem={({item}) => (
+              <AllStatements>
+                <IndividualStatement
+                  style={styles.defaultShadow}
+                  onPress={() => {
+                    navigate('Statement', {
+                      value: item.formatedValue,
+                      dueDate: item.dueDate,
+                    });
+                  }}>
+                  <StatementIcon
+                    source={item.payd ? actualIcon : scheduleIcon}
+                  />
+                  <View style={{flexDirection: 'column'}}>
+                    <StatementTitle>
+                      {item.payd ? 'Fatura paga' : 'Fatura em aberto'}
+                    </StatementTitle>
+                    <StatementPrice>
+                      {item.formatedValue}
+                      <Text style={{color: '#73a8f6'}}>
+                        {' '}
+                        / Venc. {item.dueDate}
+                      </Text>
+                    </StatementPrice>
+                  </View>
+                </IndividualStatement>
+              </AllStatements>
+            )}
+            keyExtractor={({id}, index) => id}
+          />
+        </ImageBackground>
       </Container>
     );
   }
 }
-
-export default withNavigation(Main);
 
 const styles = StyleSheet.create({
   defaultShadow: {
