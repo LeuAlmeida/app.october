aab08f23e0ce4d818149c0dd362772684c1b4bf8
LeuAlmeida <leo@webid.net.br>
2019-10-23 15:22:18 -0300

    Update routes

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,25 +1,11 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import Main from '~/pages/Main';
-import Payments from '~/pages/Payments';
-import Scheadules from '~/pages/Scheadules';
-import Contract from '~/pages/Contract';
-import Account from '~/pages/Account';
-import Indicate from '~/pages/Account';
-import MySchool from '~/pages/MySchool';
-import Interest from '~/pages/Interest';
 import Statement from '~/pages/Statement';
 
 const Routes = createAppContainer(createSwitchNavigator({
+  Statement,
   Main,
-  Payments,
-  Scheadules,
-  Contract,
-  Account,
-  Indicate,
-  MySchool,
-  Interest,
-  Statement
 }));
 
 export default Routes;
