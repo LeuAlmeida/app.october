ea0f1170a1f10ac809ace823a0ffc784f7ecf4ee
LeuAlmeida <leo@webid.net.br>
2019-10-23 14:05:33 -0300

    Create new routes

--- mobile/src/routes.js
+++ mobile/src/routes.js
@@ -1,7 +1,25 @@
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
 
 import Main from '~/pages/Main';
+import Payments from '~/pages/Payments';
+import Scheadules from '~/pages/Scheadules';
+import Contract from '~/pages/Contract';
+import Account from '~/pages/Account';
+import Indicate from '~/pages/Account';
+import MySchool from '~/pages/MySchool';
+import Interest from '~/pages/Interest';
+import Statement from '~/pages/Statement';
 
-const Routes = createAppContainer(createSwitchNavigator({ Main }));
+const Routes = createAppContainer(createSwitchNavigator({
+  Main,
+  Payments,
+  Scheadules,
+  Contract,
+  Account,
+  Indicate,
+  MySchool,
+  Interest,
+  Statement
+}));
 
 export default Routes;
