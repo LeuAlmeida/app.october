b68b1a64ab814ebd298a6a215f84cdd1664f4c44
LeuAlmeida <leo@webid.net.br>
2019-10-23 15:22:53 -0300

    First statement stylesheets

---
+++ mobile/src/pages/Statement/style.js
@@ -0,0 +1,36 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  width: 90%;
+  height: 80%;
+  margin-top: 10%;
+  align-self: center;
+`;
+
+export const CardContent = styled.View`
+
+`;
+
+export const CardIcon = styled.Image`
+
+`;
+
+export const CardTitle = styled.Text`
+
+`;
+
+export const CardDescription = styled.Text`
+
+`;
+
+export const CardValue = styled.Text`
+
+`;
+
+export const CardDate = styled.Text`
+
+`;
+
+export const CardPs = styled.Text`
+
+`;
