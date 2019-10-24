962c083bb3008e62e0af574219c514bc5632e63a
LeuAlmeida <leo@webid.net.br>
2019-10-23 21:51:13 -0300

    Ignore some yellow warnings

--- mobile/App.js
+++ mobile/App.js
@@ -3,7 +3,10 @@ import { YellowBox } from 'react-native';
 import Routes from './src/routes';
 
 YellowBox.ignoreWarnings([
-
+  'Warning: Each child',
+  'Warning: Failed prop type: Invalid props.style key',
+  'Warning: Failed prop type: Invalid prop `source`',
+  'Warning: React.createELement: type is invalid'
 ]);
 
 export default function App() {
