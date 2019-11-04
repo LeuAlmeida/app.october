8388e6b96eee38b36c0fb5662730ac578172c326
Léu Almeida <leo@webid.net.br>
2019-11-03 21:38:05 -0300

    Update application routes

---
+++ src/routes.js
@@ -0,0 +1,15 @@
+import {createAppContainer, createSwitchNavigator} from 'react-navigation';
+
+import Main from './pages/Main';
+import Statement from './pages/Statement';
+import Account from './pages/Account';
+
+const Routes = createAppContainer(
+  createSwitchNavigator({
+    Main,
+    Statement,
+    Account,
+  }),
+);
+
+export default Routes;
