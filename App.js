2acccabb1dae3e137e39402f3f620b8ea8f6e41c
Léu Almeida <leo@webid.net.br>
2019-11-03 22:34:53 -0300

    Stopping to call yellow boxes

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { YellowBox } from 'react-native';
+// import { YellowBox } from 'react-native';
 
 import Routes from './src/routes';
 
 
-YellowBox.ignoreWarnings([
-  'Warning: Each child in a list',
-  'Warning: Failed prop type: Invalid props.style key',
-  'Warning: Failed prop type: Invalid prop `source`',
-  'Warning: React.createELement: type is invalid'
-]);
+// YellowBox.ignoreWarnings([
+//   'Warning: Each child in a list',
+//   'Warning: Failed prop type: Invalid props.style key',
+//   'Warning: Failed prop type: Invalid prop `source`',
+//   'Warning: React.createELement: type is invalid'
+// ]);
 
 export default function App() {
   return <Routes />
